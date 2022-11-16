7ee79c56103324cbfc661c8527434fa05230c18b
enesyilmaz40 <59060186+enesyilmaz40@users.noreply.github.com>
2022-11-16 18:22:39 +0300

    Created screens

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ChatList from './screens/ChatList';
+import Chat from './screens/Chat';
+import SignIn from './screens/SignIn';
+import SignUp from './screens/SignUp';
+import Settings from './screens/Settings';
+import {Ionicons} from '@expo/vector-icons'
+import { Provider } from "react-native-paper";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+
+
+const TabsNavigator = () => (
+  <Tabs.Navigator screenOptions={({route})=>({
+    tabBarIcon: ({ focused, color, size }) => {
+
+      return <Ionicons name={route.name==="ChatList"?"chatbubbles":"settings"} size={size}
+       color={color} />;
+    },
+    tabBarActiveTintColor: 'tomato',
+    tabBarInactiveTintColor: 'gray',
+  })}>
+    <Tabs.Screen name="ChatList" component={ChatList}/>
+    <Tabs.Screen name="Settings" component={Settings}/>
+  </Tabs.Navigator>
+);
+
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <NavigationContainer>
+      <Provider>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={TabsNavigator} options={{headerShown:false}}/>
+        <Stack.Screen name="Chat" component={Chat}/>
+        <Stack.Screen name="SignUp" component={SignUp}/>
+        <Stack.Screen name="SignIn" component={SignIn}/>
+      </Stack.Navigator>
+      </Provider>
+    </NavigationContainer>
+  )
+}
+export default App;
